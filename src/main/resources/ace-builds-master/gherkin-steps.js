5fd058fe57713d84e22e938c466bf7e05bb41bd0
martinpiper <martin.piper@gmail.com>
2015-11-05 22:28:28 +0800

    Added run selection/current line request

--- src/main/resources/ace-builds-master/gherkin-steps.js
+++ src/main/resources/ace-builds-master/gherkin-steps.js
@@ -1,11 +1,13 @@
 // Steps that can be used for gherkin
 var simplePotentials = [
+"I have a 6502 machine",
 "Given I have a 6502 machine",
+"I assemble this file *",
 "When I assemble this file *"
 ];
 
 // Note: The indentation for snippets must be tabs not spaces!
 var complexPotentials = "\
-snippet Then the memory at address is value\n\
-	Then the memory at ${1:address} is ${2:value}\n\
+snippet the memory at address is value\n\
+	the memory at ${1:address} is ${2:value}\n\
 ";
